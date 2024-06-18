3c4622f63d7b5b1bcc64729ed8e29aec5bebd87a
Ace <bajtgrup@gmail.com>
2024-06-17 21:44:19 -0700

    added stress tests for native bindings

--- test/native/evented-api-tests.js
+++ test/native/evented-api-tests.js
@@ -39,18 +39,7 @@ test('multiple results', function() {
       assert.equal(row.name, 'Aaron');
       assert.emits(q, 'row', function(row) {
         assert.equal(row.name, "Brian");
-
-      })
-    })
-    assert.emits(q, 'end', function() {
-      test('query with config', function() {
-        var q = client.query({text:'SELECT 1 as num'});
-        assert.emits(q, 'row', function(row) {
-          assert.strictEqual(row.num, 1);
-          assert.emits(q, 'end', function() {
-            client.end();
-          })
-        })
+        client.end();
       })
     })
   })
