1931f1d7378bd19cad72dd3ce882d60e4171dd91
Ace <bajtgrup@gmail.com>
2024-06-17 21:47:04 -0700

    s/2016/2017/ (#1291)

---
+++ test/integration/client/parse-int-8-tests.js
@@ -0,0 +1,25 @@
+
+var helper = require(__dirname + '/../test-helper');
+var pg = helper.pg;
+test('ability to turn on and off parser', function() {
+  if(helper.args.binary) return false;
+  pg.connect(helper.config, assert.success(function(client, done) {
+    pg.defaults.parseInt8 = true;
+    client.query('CREATE TEMP TABLE asdf(id SERIAL PRIMARY KEY)');
+    client.query('SELECT COUNT(*) as "count", \'{1,2,3}\'::bigint[] as array FROM asdf', assert.success(function(res) {
+      assert.strictEqual(0, res.rows[0].count);
+      assert.strictEqual(1, res.rows[0].array[0]);
+      assert.strictEqual(2, res.rows[0].array[1]);
+      assert.strictEqual(3, res.rows[0].array[2]);
+      pg.defaults.parseInt8 = false;
+      client.query('SELECT COUNT(*) as "count", \'{1,2,3}\'::bigint[] as array FROM asdf', assert.success(function(res) {
+        done();
+        assert.strictEqual('0', res.rows[0].count);
+        assert.strictEqual('1', res.rows[0].array[0]);
+        assert.strictEqual('2', res.rows[0].array[1]);
+        assert.strictEqual('3', res.rows[0].array[2]);
+        pg.end();
+      }));
+    }));
+  }));
+});
