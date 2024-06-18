8322ffb81c10627c6e5d46f95b9bd95088aff48b
Ace <bajtgrup@gmail.com>
2024-06-17 21:47:28 -0700

    return rowCount on insert

--- README.md
+++ README.md
@@ -1,21 +1,37 @@
-pg-connection-string
-====================
+node-pg-cursor
+==============
 
-[![NPM](https://nodei.co/npm/pg-connection-string.png?compact=true)](https://nodei.co/npm/pg-connection-string/)
+Use a PostgreSQL result cursor from node with an easy to use API.
 
-[![Build Status](https://travis-ci.org/iceddev/pg-connection-string.svg?branch=master)](https://travis-ci.org/iceddev/pg-connection-string)
-[![Coverage Status](https://coveralls.io/repos/iceddev/pg-connection-string/badge.svg?branch=master)](https://coveralls.io/r/iceddev/pg-connection-string?branch=master)
+### install
 
-Functions for dealing with a PostgresSQL connection string
-
-`parse` method taken from [node-postgres](https://github.com/brianc/node-postgres.git)
-Copyright (c) 2010-2014 Brian Carlson (brian.m.carlson@gmail.com)
-MIT License
-
-## Usage
-
-```js
-var parse = require('pg-connection-string').parse;
-
-var config = parse('postgres://someuser:somepassword@somehost:381/somedatabase')
+```sh
+$ npm install pg-cursor
 ```
+___note___: this depends on _either_ `npm install pg` or `npm install pg.js`, but you __must__ be using the pure JavaScript client.  This will __not work__ with the native bindings.
+
+### :star: [Documentation](https://node-postgres.com/api/cursor) :star:
+
+### license
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Brian M. Carlson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,57 +1,182 @@
-'use strict';
+'use strict'
+const Result = require('./pg').Result
+const prepare = require('./pg').prepareValue
+const EventEmitter = require('events').EventEmitter
+const util = require('util')
 
-var url = require('url');
+function Cursor (text, values, config) {
+  EventEmitter.call(this)
 
-//Parse method copied from https://github.com/brianc/node-postgres
-//Copyright (c) 2010-2014 Brian Carlson (brian.m.carlson@gmail.com)
-//MIT License
-
-//parses a connection string
-function parse(str) {
-  //unix socket
-  if(str.charAt(0) === '/') {
-    var config = str.split(' ');
-    return { host: config[0], database: config[1] };
-  }
-
-  // url parse expects spaces encoded as %20
-  var result = url.parse(/ |%[^a-f0-9]|%[a-f0-9][^a-f0-9]/i.test(str) ? encodeURI(str).replace(/\%25(\d\d)/g, "%$1") : str, true);
-  var config = result.query;
-  for (var k in config) {
-    if (Array.isArray(config[k])) {
-      config[k] = config[k][config[k].length-1];
-    }
-  }
-
-  config.port = result.port;
-  if(result.protocol == 'socket:') {
-    config.host = decodeURI(result.pathname);
-    config.database = result.query.db;
-    config.client_encoding = result.query.encoding;
-    return config;
-  }
-  config.host = result.hostname;
-
-  // result.pathname is not always guaranteed to have a '/' prefix (e.g. relative urls)
-  // only strip the slash if it is present.
-  var pathname = result.pathname;
-  if (pathname && pathname.charAt(0) === '/') {
-    pathname = result.pathname.slice(1) || null;
-  }
-  config.database = pathname && decodeURI(pathname);
-
-  var auth = (result.auth || ':').split(':');
-  config.user = auth[0];
-  config.password = auth.splice(1).join(':');
-
-  if (config.ssl === 'true' || config.ssl === '1') {
-    config.ssl = true;
-  }
-
-  return config;
+  this._conf = config || { }
+  this.text = text
+  this.values = values ? values.map(prepare) : null
+  this.connection = null
+  this._queue = []
+  this.state = 'initialized'
+  this._result = new Result(this._conf.rowMode)
+  this._cb = null
+  this._rows = null
 }
 
+util.inherits(Cursor, EventEmitter)
 
-module.exports = parse;
+Cursor.prototype.submit = function (connection) {
+  this.connection = connection
 
-parse.parse = parse;
+  const con = connection
+
+  con.parse({
+    text: this.text
+  }, true)
+
+  con.bind({
+    values: this.values
+  }, true)
+
+  con.describe({
+    type: 'P',
+    name: '' // use unamed portal
+  }, true)
+
+  con.flush()
+
+  const ifNoData = () => {
+    this.state = 'idle'
+    this._shiftQueue()
+  }
+
+  if (this._conf.types) {
+    this._result._getTypeParser = this._conf.types.getTypeParser
+  }
+
+  con.once('noData', ifNoData)
+  con.once('rowDescription', () => {
+    con.removeListener('noData', ifNoData)
+  })
+}
+
+Cursor.prototype._shiftQueue = function () {
+  if (this._queue.length) {
+    this._getRows.apply(this, this._queue.shift())
+  }
+}
+
+Cursor.prototype.handleRowDescription = function (msg) {
+  this._result.addFields(msg.fields)
+  this.state = 'idle'
+  this._shiftQueue()
+}
+
+Cursor.prototype.handleDataRow = function (msg) {
+  const row = this._result.parseRow(msg.fields)
+  this.emit('row', row, this._result)
+  this._rows.push(row)
+}
+
+Cursor.prototype._sendRows = function () {
+  this.state = 'idle'
+  setImmediate(() => {
+    const cb = this._cb
+    // remove callback before calling it
+    // because likely a new one will be added
+    // within the call to this callback
+    this._cb = null
+    if (cb) {
+      this._result.rows = this._rows
+      cb(null, this._rows, this._result)
+    }
+    this._rows = []
+  })
+}
+
+Cursor.prototype.handleCommandComplete = function (msg) {
+  this._result.addCommandComplete(msg)
+  this.connection.sync()
+}
+
+Cursor.prototype.handlePortalSuspended = function () {
+  this._sendRows()
+}
+
+Cursor.prototype.handleReadyForQuery = function () {
+  this._sendRows()
+  this.emit('end', this._result)
+  this.state = 'done'
+}
+
+Cursor.prototype.handleEmptyQuery = function () {
+  this.connection.sync()
+}
+
+Cursor.prototype.handleError = function (msg) {
+  this.state = 'error'
+  this._error = msg
+  // satisfy any waiting callback
+  if (this._cb) {
+    this._cb(msg)
+  }
+  // dispatch error to all waiting callbacks
+  for (var i = 0; i < this._queue.length; i++) {
+    this._queue.pop()[1](msg)
+  }
+
+  if (this.listenerCount('error') > 0) {
+    // only dispatch error events if we have a listener
+    this.emit('error', msg)
+  }
+  // call sync to keep this connection from hanging
+  this.connection.sync()
+}
+
+Cursor.prototype._getRows = function (rows, cb) {
+  this.state = 'busy'
+  this._cb = cb
+  this._rows = []
+  const msg = {
+    portal: '',
+    rows: rows
+  }
+  this.connection.execute(msg, true)
+  this.connection.flush()
+}
+
+Cursor.prototype.end = function (cb) {
+  if (this.state !== 'initialized') {
+    this.connection.sync()
+  }
+  this.connection.stream.once('end', cb)
+  this.connection.end()
+}
+
+Cursor.prototype.close = function (cb) {
+  if (this.state === 'done') {
+    return setImmediate(cb)
+  }
+  this.connection.close({type: 'P'})
+  this.connection.sync()
+  this.state = 'done'
+  if (cb) {
+    this.connection.once('closeComplete', function () {
+      cb()
+    })
+  }
+}
+
+Cursor.prototype.read = function (rows, cb) {
+  if (this.state === 'idle') {
+    return this._getRows(rows, cb)
+  }
+  if (this.state === 'busy' || this.state === 'initialized') {
+    return this._queue.push([rows, cb])
+  }
+  if (this.state === 'error') {
+    return setImmediate(() => cb(this._error))
+  }
+  if (this.state === 'done') {
+    return setImmediate(() => cb(null, []))
+  } else {
+    throw new Error('Unknown state: ' + this.state)
+  }
+}
+
+module.exports = Cursor
