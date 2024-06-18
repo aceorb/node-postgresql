ead9a47431125d727e925ba8b4abab0b55a69c77
Ace <bajtgrup@gmail.com>
2024-06-17 21:44:15 -0700

    decrease emit timeout time

---
+++ test/libpq/binding-spike.js
@@ -0,0 +1,36 @@
+var helper = require(__dirname + "/../test-helper");
+var Connection = require(__dirname + "/../../lib/libpq").Connection;
+
+test('calling connect without params raises error', function() {
+  var con = new Connection();
+  var err;
+  try{
+    con.connect();
+  } catch (e) {
+    err = e;
+  }
+  assert.ok(err!=null);
+});
+
+test('connecting with wrong parameters', function() {
+  var con = new Connection();
+  con.connect("user=asldfkj hostaddr=127.0.0.1 port=5432 dbname=asldkfj");
+  assert.emits(con, 'error', function(error) {
+    console.log(error);
+  });
+});
+
+
+test('connects', function() {
+  var con = new Connection();
+  con.connect("user=brian hostaddr=127.0.0.1 port=5432 dbname=postgres");
+  assert.emits(con, 'connect', function() {
+    con._sendQuery("SELECT NOW()");
+    assert.emits(con, 'row', function(row) {
+      assert.ok(false, "Need to assert on row data");
+      test('ends connection', function() {
+        con.end();
+      });
+    });
+  });
+})
