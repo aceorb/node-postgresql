057d417a2531580c4b30863209aced1454f514f4
Ace <bajtgrup@gmail.com>
2024-06-17 21:46:52 -0700

    changed for self signed ssl support (#1072)

---
+++ test/integration/connection-pool/idle-timeout-tests.js
@@ -0,0 +1,14 @@
+var helper = require(__dirname + '/test-helper');
+var _ = require('lodash')
+
+const config = _.extend({ }, helper.config, { idleTimeoutMillis: 50 })
+
+test('idle timeout', function() {
+ helper.pg.connect(config, assert.calls(function(err, client, done) {
+   assert.isNull(err);
+   client.query('SELECT NOW()');
+  //just let this one time out
+  //test will hang if pool doesn't timeout
+   done();
+ }));
+});
