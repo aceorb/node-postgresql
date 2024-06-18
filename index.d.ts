24df194ed942f0a53ca4bb46774542dbccec9f1c
Ace <bajtgrup@gmail.com>
2024-06-17 21:47:27 -0700

    Fix typings

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+export function parse(connectionString: string): ConnectionOptions;
+
+export interface ConnectionOptions {
+  host: string;
+  password?: string;
+  user?: string;
+  port: string | null;
+  database: string | null | undefined;
+  client_encoding?: string | undefined;
+  ssl?: boolean;
+
+  application_name?: string;
+  fallback_application_name?: string;
+}
