c7fbd5fe5df5cdf8b82be9a5bae387c8885a85a1
Ace <bajtgrup@gmail.com>
2024-06-17 21:46:14 -0700

    Remove unused parameter 'name' from DataRowMessage.

--- test/integration/connection-pool/ending-pool-tests.js
+++ test/integration/connection-pool/ending-pool-tests.js
@@ -4,11 +4,6 @@ var called = false;
 test('disconnects', function() {
   var sink = new helper.Sink(4, function() {
     called = true;
-    var eventSink = new helper.Sink(1, function() {});
-    helper.pg.on('end', function() {
-      eventSink.add();
-    });
-
     //this should exit the process, killing each connection pool
     helper.pg.end();
   });
