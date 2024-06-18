9b77009835a9479370d9629f4c9502111f04144a
Ace <bajtgrup@gmail.com>
2024-06-17 21:45:59 -0700

    Update package.jsn

--- index.js
+++ index.js
@@ -1,107 +1,116 @@
-var assert = require('assert')
-var Readable = require('stream').Readable
-
 var path = require('path')
 
-var pgdir = false
-try {
-  pgdir = path.dirname(require.resolve('pg'))
-} catch (e) {
-  pgdir = path.dirname(require.resolve('pg.js'))
-  if(!pgdir) {
-    throw new Error("Please install either `pg` or `pg.js` to use this module")
-  }
-  pgdir = path.join(pgdir, 'lib')
-}
-var Result = require(path.join(pgdir, 'result'))
-var utils = require(path.join(pgdir, 'utils'))
+var resultPath = path.dirname(require.resolve('pg.js')) + '/lib/result'
+var Result = require(resultPath)
+var Client = require('pg.js').Client
 
-var QueryStream = module.exports = function(text, values, options) {
-  options = options || { }
-  Readable.call(this, {
-    objectMode: true,
-    highWaterMark: options.highWaterMark || 1000
-  })
+var Cursor = function(text, values) {
   this.text = text
-  assert(this.text, 'text cannot be falsy')
-  this.values = (values || []).map(utils.prepareValue)
-  this.name = ''
-  this._result = new Result()
-  this.batchSize = options.batchSize || 100
-  this._idle = true
+  this.values = values
+  this._connection = null
 }
 
-require('util').inherits(QueryStream, Readable)
+Cursor.prototype._connect = function(cb) {
+  if(this._connected) return setImmediate(cb);
+  this._connected = true
+  var self = this
+  var client = new Client()
+  client.connect(function(err) {
+    if(err) return cb(err);
 
-QueryStream.prototype._read = function(n) {
-  this._getRows(n)
+    //remove all listeners from
+    //client's connection and discard the client
+    self.connection = client.connection
+    self.connection.removeAllListeners()
+
+    var con = self.connection
+
+    con.parse({
+      text: self.text
+    }, true)
+
+    con.bind({
+      values: self.values
+    }, true)
+
+    con.describe({
+      type: 'P',
+      name: '' //use unamed portal
+    }, true)
+
+    con.flush()
+
+    var onError = function(err) {
+      cb(err)
+      con.end()
+    }
+
+    con.once('error', onError)
+
+    con.on('rowDescription', function(msg) {
+      self.rowDescription = msg
+      con.removeListener('error', onError)
+      cb(null, con)
+    })
+
+    var onRow = function(msg) {
+      var row = self.result.parseRow(msg.fields)
+      self.result.addRow(row)
+    }
+
+    con.on('dataRow', onRow)
+
+    con.once('readyForQuery', function() {
+      con.end()
+    })
+
+    con.once('commandComplete', function() {
+      self._complete = true
+      con.sync()
+    })
+  })
 }
 
-QueryStream.prototype._getRows = function(count) {
-  var con = this.connection
-  if(!this._idle || !this.connection) return;
-  this._idle = false
-  con.execute({
-    portal: '',
-    rows: count
-  }, true)
-
-  con.flush()
-}
-
-QueryStream.prototype.submit = function(con) {
-  //save reference to connection
-  this.connection = con
-
-  var name = this.name
-
-  con.parse({
-    text: this.text,
-    name: name,
-    types: []
-  }, true)
-
-  con.bind({
-    portal: '',
-    statement: name,
-    values: this.values,
-    binary: false
-  }, true)
-
-  con.describe({
-    type: 'P',
-    name: name
-  }, true)
-
-  this._getRows(this.batchSize)
-
-}
-
-QueryStream.prototype.handleRowDescription = function(msg) {
-  this._result.addFields(msg.fields)
-}
-
-QueryStream.prototype.handleDataRow = function(msg) {
-  var row = this._result.parseRow(msg.fields)
-  this._more = this.push(row)
-}
-
-QueryStream.prototype.handlePortalSuspended = function(msg) {
-  this._idle = true
-  if(this._more) {
-    this._getRows(this.batchSize)
+Cursor.prototype._getRows = function(con, n, cb) {
+  if(this._done) {
+    return cb(null, [], false)
   }
+  var msg = {
+    portal: '',
+    rows: n
+  }
+  con.execute(msg, true)
+  con.flush()
+  this.result = new Result()
+  this.result.addFields(this.rowDescription.fields)
+
+  var self = this
+
+  var onComplete = function() {
+    self._done = true
+    cb(null, self.result.rows, self.result)
+  }
+  con.once('commandComplete', onComplete)
+
+  con.once('portalSuspended', function() {
+    cb(null, self.result.rows, self.result)
+    con.removeListener('commandComplete', onComplete)
+  })
 }
 
-QueryStream.prototype.handleCommandComplete = function(msg) {
-  this.connection.sync()
+Cursor.prototype.end = function(cb) {
+  this.connection.end()
+  this.connection.stream.once('end', cb)
 }
 
-QueryStream.prototype.handleReadyForQuery = function() {
-  this.push(null)
+Cursor.prototype.read = function(rows, cb) {
+  var self = this
+  this._connect(function(err) {
+    if(err) return cb(err);
+    self._getRows(self.connection, rows, cb)
+  })
 }
 
-QueryStream.prototype.handleError = function(err) {
-  this.connection.sync()
-  this.emit('error', err)
+module.exports = function(query, params) {
+  return new Cursor(query, params)
 }
