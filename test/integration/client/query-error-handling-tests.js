71280769d8b02e3a7569c617765f062fe0df9d51
Ace <bajtgrup@gmail.com>
2024-06-17 21:46:13 -0700

    emit event when all pool are actually destroyed

--- test/integration/client/query-error-handling-tests.js
+++ test/integration/client/query-error-handling-tests.js
@@ -12,16 +12,10 @@ test('error during query execution', function() {
         pidColName = 'pid';
         queryColName = 'query';
       }
-      var query1 = client.query(sleepQuery, assert.calls(function(err, result) {
+      client.query(sleepQuery, assert.calls(function(err, result) {
         assert(err);
         client.end();
       }));
-      //ensure query1 does not emit an 'end' event
-      //because it was killed and received an error
-      //https://github.com/brianc/node-postgres/issues/547
-      query1.on('end', function() {
-        assert.fail('Query with an error should not emit "end" event')
-      })
       var client2 = new Client(helper.args);
       client2.connect(assert.success(function() {
         var killIdleQuery = "SELECT " + pidColName + ", (SELECT pg_terminate_backend(" + pidColName + ")) AS killed FROM pg_stat_activity WHERE " + queryColName + " = $1";
