3d4d61e068c0e3b71d07c5e207f4d55649dec5bf
Ace <bajtgrup@gmail.com>
2024-06-17 21:47:42 -0700

    Fix hanging listener when error occured

--- index.js
+++ index.js
@@ -1,78 +1,195 @@
-'use strict';
+'use strict'
+const Result = require('pg/lib/result.js')
+const prepare = require('pg/lib/utils.js').prepareValue
+const EventEmitter = require('events').EventEmitter
+const util = require('util')
 
-var url = require('url');
-var fs = require('fs');
+var nextUniqueID = 1 // concept borrowed from org.postgresql.core.v3.QueryExecutorImpl
 
-//Parse method copied from https://github.com/brianc/node-postgres
-//Copyright (c) 2010-2014 Brian Carlson (brian.m.carlson@gmail.com)
-//MIT License
+function Cursor (text, values, config) {
+  EventEmitter.call(this)
 
-//parses a connection string
-function parse(str) {
-  //unix socket
-  if(str.charAt(0) === '/') {
-    var config = str.split(' ');
-    return { host: config[0], database: config[1] };
-  }
-
-  // url parse expects spaces encoded as %20
-  var result = url.parse(/ |%[^a-f0-9]|%[a-f0-9][^a-f0-9]/i.test(str) ? encodeURI(str).replace(/\%25(\d\d)/g, "%$1") : str, true);
-  var config = result.query;
-  for (var k in config) {
-    if (Array.isArray(config[k])) {
-      config[k] = config[k][config[k].length-1];
-    }
-  }
-
-  var auth = (result.auth || ':').split(':');
-  config.user = auth[0];
-  config.password = auth.splice(1).join(':');
-
-  config.port = result.port;
-  if(result.protocol == 'socket:') {
-    config.host = decodeURI(result.pathname);
-    config.database = result.query.db;
-    config.client_encoding = result.query.encoding;
-    return config;
-  }
-  config.host = result.hostname;
-
-  // result.pathname is not always guaranteed to have a '/' prefix (e.g. relative urls)
-  // only strip the slash if it is present.
-  var pathname = result.pathname;
-  if (pathname && pathname.charAt(0) === '/') {
-    pathname = result.pathname.slice(1) || null;
-  }
-  config.database = pathname && decodeURI(pathname);
-
-  if (config.ssl === 'true' || config.ssl === '1') {
-    config.ssl = true;
-  }
-
-  if (config.ssl === '0') {
-    config.ssl = false;
-  }
-
-  if (config.sslcert || config.sslkey || config.sslrootcert) {
-    config.ssl = {};
-  }
-
-  if (config.sslcert) {
-    config.ssl.cert = fs.readFileSync(config.sslcert).toString();
-  }
-
-  if (config.sslkey) {
-    config.ssl.key = fs.readFileSync(config.sslkey).toString();
-  }
-
-  if (config.sslrootcert) {
-    config.ssl.ca = fs.readFileSync(config.sslrootcert).toString();
-  }
-
-  return config;
+  this._conf = config || {}
+  this.text = text
+  this.values = values ? values.map(prepare) : null
+  this.connection = null
+  this._queue = []
+  this.state = 'initialized'
+  this._result = new Result(this._conf.rowMode)
+  this._cb = null
+  this._rows = null
+  this._portal = null
+  this._ifNoData = this._ifNoData.bind(this)
+  this._rowDescription = this._rowDescription.bind(this)
 }
 
+util.inherits(Cursor, EventEmitter)
 
-module.exports = parse;
+Cursor.prototype._ifNoData = function () {
+  this.state = 'idle'
+  this._shiftQueue()
+}
 
-parse.parse = parse;
+Cursor.prototype._rowDescription = function () {
+  if (this.connection) {
+    this.connection.removeListener('noData', this._ifNoData)
+  }
+}
+
+Cursor.prototype.submit = function (connection) {
+  this.connection = connection
+  this._portal = 'C_' + (nextUniqueID++)
+
+  const con = connection
+
+  con.parse({
+    text: this.text
+  }, true)
+
+  con.bind({
+    portal: this._portal,
+    values: this.values
+  }, true)
+
+  con.describe({
+    type: 'P',
+    name: this._portal // AWS Redshift requires a portal name
+  }, true)
+
+  con.flush()
+
+  if (this._conf.types) {
+    this._result._getTypeParser = this._conf.types.getTypeParser
+  }
+
+  con.once('noData', this._ifNoData)
+  con.once('rowDescription', this._rowDescription)
+}
+
+Cursor.prototype._shiftQueue = function () {
+  if (this._queue.length) {
+    this._getRows.apply(this, this._queue.shift())
+  }
+}
+
+Cursor.prototype.handleRowDescription = function (msg) {
+  this._result.addFields(msg.fields)
+  this.state = 'idle'
+  this._shiftQueue()
+}
+
+Cursor.prototype.handleDataRow = function (msg) {
+  const row = this._result.parseRow(msg.fields)
+  this.emit('row', row, this._result)
+  this._rows.push(row)
+}
+
+Cursor.prototype._sendRows = function () {
+  this.state = 'idle'
+  setImmediate(() => {
+    const cb = this._cb
+    // remove callback before calling it
+    // because likely a new one will be added
+    // within the call to this callback
+    this._cb = null
+    if (cb) {
+      this._result.rows = this._rows
+      cb(null, this._rows, this._result)
+    }
+    this._rows = []
+  })
+}
+
+Cursor.prototype.handleCommandComplete = function (msg) {
+  this._result.addCommandComplete(msg)
+  this.connection.sync()
+}
+
+Cursor.prototype.handlePortalSuspended = function () {
+  this._sendRows()
+}
+
+Cursor.prototype.handleReadyForQuery = function () {
+  this._sendRows()
+  this.emit('end', this._result)
+  this.state = 'done'
+}
+
+Cursor.prototype.handleEmptyQuery = function () {
+  this.connection.sync()
+}
+
+Cursor.prototype.handleError = function (msg) {
+  this.connection.removeListener('noData', this._ifNoData)
+  this.connection.removeListener('rowDescription', this._rowDescription)
+  this.state = 'error'
+  this._error = msg
+  // satisfy any waiting callback
+  if (this._cb) {
+    this._cb(msg)
+  }
+  // dispatch error to all waiting callbacks
+  for (var i = 0; i < this._queue.length; i++) {
+    this._queue.pop()[1](msg)
+  }
+
+  if (this.listenerCount('error') > 0) {
+    // only dispatch error events if we have a listener
+    this.emit('error', msg)
+  }
+  // call sync to keep this connection from hanging
+  this.connection.sync()
+}
+
+Cursor.prototype._getRows = function (rows, cb) {
+  this.state = 'busy'
+  this._cb = cb
+  this._rows = []
+  const msg = {
+    portal: this._portal,
+    rows: rows
+  }
+  this.connection.execute(msg, true)
+  this.connection.flush()
+}
+
+Cursor.prototype.end = function (cb) {
+  if (this.state !== 'initialized') {
+    this.connection.sync()
+  }
+  this.connection.once('end', cb)
+  this.connection.end()
+}
+
+Cursor.prototype.close = function (cb) {
+  if (this.state === 'done') {
+    return setImmediate(cb)
+  }
+  this.connection.close({ type: 'P' })
+  this.connection.sync()
+  this.state = 'done'
+  if (cb) {
+    this.connection.once('closeComplete', function () {
+      cb()
+    })
+  }
+}
+
+Cursor.prototype.read = function (rows, cb) {
+  if (this.state === 'idle') {
+    return this._getRows(rows, cb)
+  }
+  if (this.state === 'busy' || this.state === 'initialized') {
+    return this._queue.push([rows, cb])
+  }
+  if (this.state === 'error') {
+    return setImmediate(() => cb(this._error))
+  }
+  if (this.state === 'done') {
+    return setImmediate(() => cb(null, []))
+  } else {
+    throw new Error('Unknown state: ' + this.state)
+  }
+}
+
+module.exports = Cursor
