ac7020c12a203841629f92ae9c3298e4a278b619
Ace <bajtgrup@gmail.com>
2024-06-17 21:44:29 -0700

    updated readme

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,14 +1,15 @@
 require.paths.unshift(__dirname + '/../lib/');
-
-Client = require('client');
-EventEmitter = require('events').EventEmitter;
-
-sys = require('sys');
+//make assert a global...
 assert = require('assert');
-BufferList = require(__dirname+'/buffer-list')
-buffers = require(__dirname + '/test-buffers');
-Connection = require('connection');
+
+var EventEmitter = require('events').EventEmitter;
+var sys = require('sys');
+var BufferList = require(__dirname+'/buffer-list')
+
+var Connection = require('connection');
 var args = require(__dirname + '/cli');
+
+Client = require(__dirname + '/../lib').Client;
 
 process.on('uncaughtException', function(d) {
   if ('stack' in d && 'message' in d) {
@@ -32,7 +33,7 @@ assert.emits = function(item, eventName, callback) {
     test("Should have called " + eventName, function() {
       assert.ok(called, "Expected '" + eventName + "' to be called.")
     });
-  },20000);
+  },2000);
 
   item.once(eventName, function() {
     called = true;
@@ -95,6 +96,14 @@ assert.success = function(callback) {
   })
 }
 
+assert.throws = function(offender) {
+  try {
+    offender();
+  } catch (e) {
+    return;
+  }
+  assert.ok(false, "Expected " + offender + " to throw exception");
+}
 
 assert.length = function(actual, expectedLength) {
   assert.equal(actual.length, expectedLength);
@@ -205,7 +214,9 @@ module.exports = {
   pg: require('index'),
   connectionString: function() {
     return "pg"+(count++)+"://"+args.user+":"+args.password+"@"+args.host+":"+args.port+"/"+args.database;
-  }
+  },
+  sys: sys,
+  Client: Client
 };
 
 
